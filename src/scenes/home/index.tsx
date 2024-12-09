b09b545e485febcd6cf80f912f7d4c89233059da
denisdiaconu <denisdiaconu117@yahoo.com>
2024-12-09 11:20:59 +0100

    Add base layout

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -6,13 +6,46 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Home = ({ setSelectedPage }: Props) => {
-  return <div>Home</div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p>
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </div>
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              href={`#${SelectedPage.ContactUs}`}
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            ><p>Learn More</p></AnchorLink>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
