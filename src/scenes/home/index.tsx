098f770880fa9c248fd72b0ea05e9b839419fbac
denisdiaconu <denisdiaconu117@yahoo.com>
2024-12-09 10:58:44 +0100

    Add images for home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,4 +1,11 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
