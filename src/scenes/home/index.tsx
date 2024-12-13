a11327bd06ce0a6fba01f53e4b0d29be85d1df5a
denisdiaconu <denisdiaconu117@yahoo.com>
2024-12-13 11:38:14 +0100

    Add motion to home link

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -18,7 +18,10 @@ const Home = ({ setSelectedPage }: Props) => {
 
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         <div className="z-10 mt-32 md:basis-3/5">
           <motion.div
             className="md:-mt-20"
@@ -68,7 +71,7 @@ const Home = ({ setSelectedPage }: Props) => {
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
-      </div>
+      </motion.div>
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
