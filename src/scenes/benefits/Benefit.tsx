1a552d2b7fd35e74b495063b9880f4be5ebcd775
denisdiaconu <denisdiaconu117@yahoo.com>
2024-12-18 11:12:16 +0100

    Add Benefit to index

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,10 +1,11 @@
 import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   icon: JSX.Element;
   title: string;
   description: string;
-  setSelectedPage: ( value: SelectedPage ) => void;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
@@ -15,6 +16,15 @@ const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
           {icon}
         </div>
       </div>
+      <h4 className="font-bold">{title}</h4>
+      <p className="my-3">{description}</p>
+      <AnchorLink
+        className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+        href={`#${SelectedPage.ContactUs}`}
+        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        <p>Learn More</p>
+      </AnchorLink>
     </div>
   );
 };
