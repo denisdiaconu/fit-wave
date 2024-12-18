d9066a5059a0a09769a28c94acf4f6fd55aa5316
denisdiaconu <denisdiaconu117@yahoo.com>
2024-12-18 11:05:38 +0100

    Add Benefit types

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { SelectedPage } from "@/shared/types";
 
-type Props = {}
+type Props = {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+  setSelectedPage: ( value: SelectedPage ) => void;
+};
 
-const Benefit = (props: Props) => {
+const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div>
-        
+    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+      <div className="mb-4 flex justify-center">
+        <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
+          {icon}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Benefit
+export default Benefit;
