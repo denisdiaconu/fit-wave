39d12569ca8debd49cd44bb7b6ab4b4355bb16bc
denisdiaconu <denisdiaconu117@yahoo.com>
2024-12-25 10:40:59 +0100

    Add images to ourClasses

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { SelectedPage } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
 
-type Props = {}
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const OurClasses = (props: Props) => {
-  return (
-    <div>OurClasses</div>
-  )
-}
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return <div>OurClasses</div>;
+};
 
-export default OurClasses
+export default OurClasses;
