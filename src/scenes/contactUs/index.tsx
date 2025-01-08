8797d20195e92360a53c8b31ff9e3c0444a73c7c
denisdiaconu <denisdiaconu117@yahoo.com>
2025-01-08 10:49:55 +0100

    Add contactUs header animation

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,16 +1,35 @@
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
-import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png"
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  return <section id="contactus">
-
-  </section>
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        ><HText>
+          <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText></motion.div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
