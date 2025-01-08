91fc12ea977f6b3a4c3702df5a76864486df2200
denisdiaconu <denisdiaconu117@yahoo.com>
2025-01-08 10:56:32 +0100

    Add contactUs form animation

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -24,9 +24,29 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
           }}
-        ><HText>
-          <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
-          </HText></motion.div>
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+          </p>
+        </motion.div>
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          ></motion.div>
+        </div>
       </motion.div>
     </section>
   );
