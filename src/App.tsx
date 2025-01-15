948dd6d4aded42a4b258f7ccac199bb3f3a338c4
denisdiaconu <denisdiaconu117@yahoo.com>
2025-01-15 11:07:03 +0100

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
+import Footer from "@/scenes/footer";
 import ContactUs from "@/scenes/contactUs";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
@@ -36,6 +37,7 @@ function App() {
         <Benefits setSelectedPage={setSelectedPage} />
         <OurClasses setSelectedPage={setSelectedPage} />
         <ContactUs setSelectedPage={setSelectedPage} />
+        <Footer />
       </div>
     </>
   );
